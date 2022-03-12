71c20e20ebc0a470a16f98d299fc5d9dcab48282
PabloHenrique <pablohenriquenadai@gmail.com>
2022-03-12 20:50:05 -0300

    2022 03 12 - Aula 01

---
+++ Aula01/script.js
@@ -0,0 +1,18 @@
+var nome = "Pablo_Henrique"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 9 
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso é um comentário 
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
